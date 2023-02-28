a9f58ff26a4074df1d9ca8ab50e63ce7cf533b0d
lucdalri <125897264+lucdalri@users.noreply.github.com>
2023-02-28 10:46:09 -0300

    Update RemoverHTML.py

--- RemoverHTML.py
+++ RemoverHTML.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[38]:
-
-
 import pandas as pd
 from bs4 import BeautifulSoup
 
-#Definindo o id do arquivo:
+#Definindo a identificação do arquivo:
 id = input('Qual o id?')
-
-# Importando o arquivo CSV com código HTML:
-df = pd.read_csv(r'C:\Users\lucas\Downloads\OBSERV.csv', encoding='iso-8859-1')
 
 # Definindo a função RemoverHTML:
 def RemoverHTML(texto):
@@ -21,16 +12,15 @@ def RemoverHTML(texto):
     else:
         return texto
 
+# Importando o arquivo CSV com código HTML:
+df = pd.read_csv(r'CAMINHO DO ARQUIVO', encoding='iso-8859-1')
+
 # Aplicar a função a todos os elementos do DataFrame:
 df = df.applymap(RemoverHTML)
 
-# Exportar o arquivo sem HTML:
+# Exportar o arquivo em formato excel sem entidades HTML:
 NomeArquivo = id + '.xlsx'
 df.to_excel(NomeArquivo, index=False)
 
 #Indicação de finalização:
 print('Pronto!')
-
-
-
-
